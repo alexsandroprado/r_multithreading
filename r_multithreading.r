e0e21a7e0f277487cf751d471562114f69987ebb
Alexsandro Prado <30550802+alexsandroprado@users.noreply.github.com>
2022-05-02 23:34:32 -0300

    R multithreading
    Ativar o todos os núcleos disponíveis do processador dentro do R.

---
+++ r_multithreading.r
@@ -0,0 +1,10 @@
+# Pacotes necessarios ----
+if (!require('doParallel')) install.packages('doParallel', dependencies = TRUE); library('doParallel')
+
+# Ativar Multithending no R puro----
+library(doParallel)
+cl <- makeCluster(detectCores()-1)
+registerDoParallel(cl)
+showConnections() #Checar se o multicore esta ON
+for (node in cl) try(print(node)) #Checar se o multicore esta ON
+#stopCluster(cl) ##Desativar multiprocessamento
