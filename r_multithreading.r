8e019f8d697509ac8b84cae430dd0663f0259c55
Alexsandro Prado <30550802+alexsandroprado@users.noreply.github.com>
2022-05-02 23:58:20 -0300

    Update r_multithreading.r

--- r_multithreading.r
+++ r_multithreading.r
@@ -1,3 +1,4 @@
+print("## ATIVANDO MULTIPROCESSAMENTO NO AMBIENTE R ##")
 # Pacotes necessarios ----
 if (!require('doParallel')) install.packages('doParallel', dependencies = TRUE); library('doParallel')
 
@@ -8,6 +9,6 @@ registerDoParallel(cl)
 showConnections() #Checar se o multicore esta ON
 for (node in cl) try(print(node)) #Checar se o multicore esta ON
 #stopCluster(cl) ##Desativar multiprocessamento
+
 print("@contabilidades | @ufersa3 |@gecomt_ufersa")
 print("https://github.com/alexsandroprado");print("“In God we trust; all others must bring data” - Deming")
-
