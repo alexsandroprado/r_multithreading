7fe4ccc98e092b7128245f3bfe0aa9f12c242a8e
Alexsandro Prado <30550802+alexsandroprado@users.noreply.github.com>
2022-05-02 23:58:51 -0300

    Update r_multithreading.r

--- r_multithreading.r
+++ r_multithreading.r
@@ -10,5 +10,5 @@ showConnections() #Checar se o multicore esta ON
 for (node in cl) try(print(node)) #Checar se o multicore esta ON
 #stopCluster(cl) ##Desativar multiprocessamento
 
-print("@contabilidades | @ufersa3 |@gecomt_ufersa")
+print("@contabilidades | @ufersa3 | @gecomt_ufersa")
 print("https://github.com/alexsandroprado");print("“In God we trust; all others must bring data” - Deming")
